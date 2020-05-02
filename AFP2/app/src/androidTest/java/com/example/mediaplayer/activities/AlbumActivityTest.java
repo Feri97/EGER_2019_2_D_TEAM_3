f4670d78daaf9c7deb6fa4ff1ba89ff6c55add8a
Archerial <agadam98@gmail.com>
2020-05-02 12:02:36 +0200

    AlbumActivity test
    Album activity alap teszt fájl létrehozása

--- AFP2/app/src/androidTest/java/com/example/mediaplayer/activities/AlbumActivityTest.java
+++ AFP2/app/src/androidTest/java/com/example/mediaplayer/activities/AlbumActivityTest.java
@@ -1,6 +1,5 @@
 package com.example.mediaplayer.activities;
 
-import android.app.Instrumentation;
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.view.View;
@@ -12,48 +11,40 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static junit.framework.TestCase.assertNotNull;
-import static junit.framework.TestCase.assertNull;
-import static junit.framework.TestCase.assertTrue;
-import static android.support.test.espresso.Espresso.onView;
+import static org.junit.Assert.*;
 
-
-public class MainActivityTest extends MainActivity {
+public class AlbumActivityTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule= new ActivityTestRule<MainActivity>(MainActivity.class);
-    private MainActivity mActivity=null;
+    public ActivityTestRule<AlbumActivity> aActivityTestRule= new ActivityTestRule<AlbumActivity>(AlbumActivity.class);
+    private AlbumActivity aActivity=null;
 
 
     @Before
     public void setUp() throws Exception {
-
-        mActivity=mActivityTestRule.getActivity();
+        aActivity=aActivityTestRule.getActivity();
     }
 
     @Test
     public void test1() {
-        View view=mActivity.findViewById(R.id.mini_player_title);
+        View view=aActivity.findViewById(R.id.mini_player_title);
         assertNotNull(view);
     }
 
     @Test
     public void test2(){
-        mActivityTestRule.launchActivity(new Intent());
-
+        aActivityTestRule.launchActivity(new Intent());
         onView(withId(R.id.mini_player_image)).perform(click());
     }
 
 
     @After
     public void tearDown() throws Exception {
-
-        mActivity=null;
+        aActivity = null;
     }
-
 
 }
